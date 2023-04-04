e3bf9cad3373a339d7ba03a2b7c92c8a49e913e1
Stepalom77 <stepalom22@gmail.com>
2023-04-04 18:35:22 -0500

    modificar y eliminar reviews y movies agregado

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,3 +1,8 @@
+//id state
+
+let movieId;
+let reviewId;
+
 //Get Reviews
 reviews()
 
@@ -8,8 +13,11 @@ async function getAllReviews() {
 }
 
 function showReviews() {
+    document.getElementById('create-review').hidden = true;
     document.getElementById('movies').hidden = true;
     document.getElementById('create-movie').hidden = true;
+    document.getElementById('update-movie').hidden = true;
+    document.getElementById('update-review').hidden = true;
     document.getElementById('reviews').hidden = false;
 }
 
@@ -29,7 +37,10 @@ async function reviews() {
                     <div class="card-body">
                         <h5 class="card-title">${element.username}</h5>
                         <h6>Película: ${element.movieTitle}</h6>
-                        <p class="card-text">${element.description}</p>
+                        <p class="card-text mb-3">${element.description}</p>
+                        <div class="text-center ">
+                        <div class="btn btn-sm" onclick="showUpdateReview(${element.id})" id="button">Modificar Reseña</div>
+                      </div>
                     </div>
                 </div>`;
                 reviewsPanel.insertAdjacentHTML("afterbegin", cards);
@@ -45,8 +56,11 @@ async function getAllMovies() {
 }
 
 function showMovies() {
+    document.getElementById('create-review').hidden = true;
     document.getElementById('reviews').hidden = true;
     document.getElementById('create-movie').hidden = true;
+    document.getElementById('update-movie').hidden = true;
+    document.getElementById('update-review').hidden = true;
     document.getElementById('movies').hidden = false;
 }
 
@@ -66,7 +80,10 @@ async function movies() {
                     <h5 class="card-title">${element.title}</h5>
                     <div class="card-body">
                         <p class="card-text">${element.description}</p>
-                        <img src="${element.image}" class="card-img">
+                        <img src="${element.image}" class="card-img mb-3" style="width: 10rem; height: 10rem;" >
+                        <div class="text-center ">
+                        <div class="btn btn-sm" onclick="showUpdateMovie(${element.id})" id="button">Modificar Película</div>
+                      </div>
                     </div>
                 </div>`;
                 moviesPanel.insertAdjacentHTML("afterbegin", cards);
@@ -116,15 +133,18 @@ async function createMovie() {
             alert(data.error);
             return;
         } else {
-            alert('Ha agregado el producto satisfactoriamente!')
+            alert('Ha agregado la película satisfactoriamente!')
             cleanCreateMovie();
         }
     }
 }
 
 function showCreateMovie() {
+    document.getElementById('create-review').hidden = true;
     document.getElementById('reviews').hidden = true;
     document.getElementById('movies').hidden = true;
+    document.getElementById('update-movie').hidden = true;
+    document.getElementById('update-review').hidden = true;
     document.getElementById('create-movie').hidden = false;
     document.getElementById('movie-title').value = '';
     document.getElementById('movie-description').value = '';
@@ -136,4 +156,251 @@ function cleanCreateMovie() {
     document.getElementById('movie-title').value = '';
     document.getElementById('movie-description').value = '';
     document.getElementById('movie-image').value = '';
+}
+
+//Modificar película
+
+function submitFormMovieUpdate() {
+    const newMovieTitle = document.getElementById('movie-title-update').value;
+    const newMovieDescription = document.getElementById('movie-description-update').value;
+    const movieImagePath = document.getElementById('movie-image-update').value + '';
+    const splittedPath = movieImagePath.split('\\');
+    const newMovieImage = `assets/img/${splittedPath[splittedPath.length - 1]}`;
+
+    const movie = {}
+
+    if(newMovieTitle) {
+        movie['title'] = newMovieTitle
+    }
+    if(newMovieDescription) {
+        movie['description'] = newMovieDescription
+    }
+    if(movieImagePath) {
+        movie['image'] = newMovieImage
+    }
+
+    return movie;
+}
+
+async function updateMovie() {
+    const movieUpdated = submitFormMovieUpdate();
+
+    if (movieUpdated) {
+
+        const response = await fetch(`http://localhost:5000/api/movies/${movieId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movieUpdated)
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+            alert(data.error);
+            return;
+        } else {
+            alert('Ha actualizado la película satisfactoriamente!')
+            cleanUpdateMovie();
+        }
+    }
+}
+
+function showUpdateMovie(id) {
+    document.getElementById('create-movie').hidden = true;
+    document.getElementById('create-review').hidden = true;
+    document.getElementById('reviews').hidden = true;
+    document.getElementById('movies').hidden = true;
+    document.getElementById('update-review').hidden = true;
+    document.getElementById('update-movie').hidden = false;
+    document.getElementById('movie-title-update').value = '';
+    document.getElementById('movie-description-update').value = '';
+    document.getElementById('movie-image-update').value = '';
+    movieId = id
+    cleanUpdateMovie()
+}
+
+function cleanUpdateMovie() {
+    document.getElementById('movie-title-update').value = '';
+    document.getElementById('movie-description-update').value = '';
+    document.getElementById('movie-image-update').value = '';
+}
+
+//Delete movie
+
+async function deleteMovie () {
+    const response = await fetch(`http://localhost:5000/api/movies/${movieId}`, {
+        method: 'DELETE',
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+        alert(data.error);
+        return;
+    } else {
+        alert('Ha borrado la película satisfactoriamente!')
+    }
+}
+
+//Escribir review
+
+function submitFormReview() {
+    const newReviewUser = document.getElementById('review-user').value;
+    const newReviewTitle = document.getElementById('review-title').value;
+    const newMovieReviewTitle = document.getElementById('review-movie-title').value;
+    const newReviewDescription = document.getElementById('review-description').value;
+
+    if (!newReviewUser || !newReviewTitle || !newMovieReviewTitle || !newReviewDescription) {
+        alert('Debe completar todo el formulario para crear su reseña')
+        return null;
+    }
+
+    const newReview = {
+        username: newReviewUser,
+        title: newReviewTitle,
+        movieTitle: newMovieReviewTitle,
+        description: newReviewDescription
+    }
+
+    return newReview;
+}
+
+async function createReview() {
+
+    const newReview = submitFormReview();
+
+    if (newReview) {
+
+        const response = await fetch('http://localhost:5000/api/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newReview)
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+            alert(data.error);
+            return;
+        } else {
+            alert('Ha agregado su reseña satisfactoriamente!')
+            cleanCreateReview();
+        }
+    }
+}
+
+function showCreateReview() {
+    document.getElementById('reviews').hidden = true;
+    document.getElementById('movies').hidden = true;
+    document.getElementById('create-movie').hidden = true;
+    document.getElementById('update-movie').hidden = true;
+    document.getElementById('update-review').hidden = true;
+    document.getElementById('create-review').hidden = false;
+    document.getElementById('review-user').value = '';
+    document.getElementById('review-title').value = '';
+    document.getElementById('review-movie-title').value = '';
+    document.getElementById('review-description').value = '';
+    cleanCreateReview()
+}
+
+function cleanCreateReview() {
+    document.getElementById('review-user').value = '';
+    document.getElementById('review-title').value = '';
+    document.getElementById('review-movie-title').value = '';
+    document.getElementById('review-description').value = '';
+}
+
+//Modificar review
+
+function submitFormReviewUpdate() {
+    const newReviewUser = document.getElementById('review-user-update').value;
+    const newReviewTitle = document.getElementById('review-title-update').value;
+    const newMovieReviewTitle = document.getElementById('review-movie-title-update').value;
+    const newReviewDescription = document.getElementById('review-description-update').value;
+
+    const review = {}
+
+    if(newReviewUser) {
+        review['username'] = newReviewUser
+    }
+    if(newReviewTitle) {
+        review['title'] = newReviewTitle
+    }
+    if(newMovieReviewTitle) {
+        review['movieTitle'] = newMovieReviewTitle
+    }
+    if(newReviewDescription) {
+        review['description'] = newReviewDescription
+    }
+
+    return review;
+}
+
+async function updateReview() {
+    const reviewUpdated = submitFormReviewUpdate();
+    console.log(reviewUpdated)
+
+    if (reviewUpdated) {
+
+        const response = await fetch(`http://localhost:5000/api/reviews/${reviewId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reviewUpdated)
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+            alert(data.error);
+            return;
+        } else {
+            alert('Ha actualizado la reseña satisfactoriamente!')
+            cleanUpdateReview();
+        }
+    }
+}
+
+function showUpdateReview(id) {
+    document.getElementById('create-movie').hidden = true;
+    document.getElementById('create-review').hidden = true;
+    document.getElementById('reviews').hidden = true;
+    document.getElementById('movies').hidden = true;
+    document.getElementById('update-movie').hidden = true;
+    document.getElementById('update-review').hidden = false;
+    document.getElementById('review-user-update').value = '';
+    document.getElementById('review-title-update').value = '';
+    document.getElementById('review-movie-title-update').value = '';
+    document.getElementById('review-description-update').value = '';
+    reviewId = id
+    cleanUpdateReview()
+}
+
+function cleanUpdateReview() {
+    document.getElementById('review-user-update').value = '';
+    document.getElementById('review-title-update').value = '';
+    document.getElementById('review-movie-title-update').value = '';
+    document.getElementById('review-description-update').value = '';
+}
+
+//Delete review
+
+async function deleteReview () {
+    const response = await fetch(`http://localhost:5000/api/reviews/${reviewId}`, {
+        method: 'DELETE',
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+        alert(data.error);
+        return;
+    } else {
+        alert('Ha borrado la reseña satisfactoriamente!')
+    }
 }
